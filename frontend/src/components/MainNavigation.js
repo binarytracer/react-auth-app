0a8e75b19e592ea367427b84797256507a37f896
Rod <gohetiaroderick@gmail.com>
2025-01-03 23:17:26 +0800

    chore: completion

---
+++ frontend/src/components/MainNavigation.js
@@ -0,0 +1,71 @@
+import { Form, NavLink, useRouteLoaderData } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup";
+
+function MainNavigation() {
+  const isLogin = useRouteLoaderData("root");
+
+  console.log({ isLogin });
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/events"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Events
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/newsletter"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Newsletter
+            </NavLink>
+          </li>
+          {!isLogin && (
+            <li>
+              <NavLink
+                to="/auth?mode=login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Authentication
+              </NavLink>
+            </li>
+          )}
+          {isLogin && (
+            <li>
+              <Form method="post" action="/logout">
+                <button>Logout</button>
+              </Form>
+            </li>
+          )}
+        </ul>
+      </nav>
+      <NewsletterSignup />
+    </header>
+  );
+}
+
+export default MainNavigation;
